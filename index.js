75424c1e04a11dab17bcf0f578a5696cc93a1782
Rajeswaran <rajeswaran149@gmail.com>
2024-09-06 17:30:16 +0530

    connected database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+require("dotenv").config();
+
 app.use('/', (req,res) => {
     res.send("server is running sucessfully")
 })
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.error('MongoDB connection error:', error));
+
 
 app.listen(5000, () => console.log('Server running on port 5000'));
